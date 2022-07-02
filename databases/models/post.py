621cf733c7f0f2433dc4e0f06128dfc75c688154
Pavlo Lankmilier <pavlo.lankmilier@onix-systems.com>
2022-07-02 16:38:17 +0300

    feat: Linting

--- databases/models/post.py
+++ databases/models/post.py
@@ -11,25 +11,23 @@ class Post(BaseModel): # pylint: disable=too-few-public-methods
     """
     table_name = 'post'
     manager_class = BaseManager
-    foreign_keys = {
-        'photo_posts': 'post_id'
-    }
-    
+  
     def photo_posts(self):
         """
             Getting many-to-many photos
         """
         # PhotoPost
-        # 
-        return PhotoPost.objects.filter(post_id=self.primary_key)
+        return PhotoPost.objects.filter(post_id=getattr(self, self.primary_key))
 
-    
+
     def prefetch():
         # JOIN
         pass
 
 
 class PhotoPost:
+    post = ForeignKey(Post, 'post_id', 'id')
+    photo = ForeignKey(Photo, 'post_id', 'id')
 
     @property
     def post(cls):
@@ -47,20 +45,21 @@ class PhotoPost:
 # 4. limit
 # 5. offset
 
+# Post.objects.prefetch('photo_posts').photo_posts.prefetch('photo').first().photo
 
 ## filter -> sql WHERE 
 
-Post.objects.first().photo_posts().first().photo -> Manager -> all, first, last -> filter
+# Post.objects.first().photo_posts().first().photo -> Manager -> all, first, last -> filter
 
 
 
 
-Post.objects.where(id=10).photos().values('photo')
-PhotoPost.objects.all() -> # SELECT * FROM photo_post -> [PhotoPost, PhotoPost]
-PhotoPost.objects.all().values('photo') -> # [Photo, Photo]
-PhotoPost.objects.all().values('post') -> # [Post, Post]
+# Post.objects.where(id=10).photos().values('photo')
+# PhotoPost.objects.all() -> # SELECT * FROM photo_post -> [PhotoPost, PhotoPost]
+# PhotoPost.objects.all().values('photo') -> # [Photo, Photo]
+# PhotoPost.objects.all().values('post') -> # [Post, Post]
 
 
-photo_post = PhotoPost.objects.first()
-post = photo_post.post -> Post
-photo = photo_post.photo -> Photo
+# photo_post = PhotoPost.objects.first()
+# post = photo_post.post -> Post
+# photo = photo_post.photo -> Photo
