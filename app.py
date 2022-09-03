69db14d9f444a035f55a03a7149be37923668673
Dolzhenkov-Andrii <oscurabow@gmail.com>
2022-09-03 17:42:57 +0300

    POST request

--- app.py
+++ app.py
@@ -5,9 +5,7 @@
 from flask import Flask
 from flask_cors import CORS
 # , cross_origin
-from appli import posts
-from appli import users
-from appli import photos
+from appli import posts,users,photos,authorization
 from config.db import DB_URI, db
 
 app = Flask(__name__)
@@ -20,6 +18,7 @@ CORS(app, support_credentials=True)
 app.register_blueprint(posts.posts, url_prefix='/api/')
 app.register_blueprint(users.users ,url_prefix='/api/')
 app.register_blueprint(photos.photos ,url_prefix='/api/')
+app.register_blueprint(authorization.author ,url_prefix='/api/')
 
 if __name__=="__main__":
     app.run('127.0.0.1',5050, debug=True)
---
+++ appli/authorization.py
@@ -0,0 +1,25 @@
+"""
+    Authorization date
+"""
+
+
+
+
+
+from flask import Blueprint, request
+
+author = Blueprint('author', __name__, template_folder='templates')
+
+@author.route('/authorization', methods=['POST'])
+def autho_form():
+    """
+        Authorization form
+    """
+    # print(uuid)
+    authoForm = request.get_json()
+    tmp2 = {0: f'{authoForm}', 1: 'test',}
+    if (authoForm['login']=='test'):
+        tmp2 = {1:'YES',}
+    else:
+        tmp2 = {1:'NO',}
+    return str(tmp2)
